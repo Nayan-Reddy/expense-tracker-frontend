8e44749c16edccfda20484af69596d3379e6c37e
Nayan Reddy <nayanreddyvit@gmail.com>
2025-07-03 20:07:41 +0530

    Add mothly analytics tab in front

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from add_update_ui import add_update_tab
 from analytics_ui import analytics_tab
-
+from monthly_analytics_ui import monthly_analytics_tab
 
 st.title("Expense Tracking System")
 
-tab1, tab2 = st.tabs(["Add/Update", "Analytics"])
+tab1, tab2, tab3 = st.tabs(["Add/Update", "Analytics", "Monthly Analytics"])
 
 with tab1:
     add_update_tab()
@@ -13,4 +13,5 @@ with tab1:
 with tab2:
     analytics_tab()
 
-
+with tab3:
+    monthly_analytics_tab() 
---
+++ monthly_analytics_ui.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+import pandas as pd
+from datetime import datetime
+
+API_URL = "https://expense-tracker-backend-t4tx.onrender.com"
+
+
+def monthly_analytics_tab():
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("Start Date", datetime(2025, 1, 3), key="start_monthly")
+    with col2:
+        end_date = st.date_input("End Date", datetime(2025, 6, 30), key="end_monthly")
+
+    if st.button("Get Monthly Analytics"):
+        payload = {
+            "start_date": start_date.strftime("%Y-%m-%d"),
+            "end_date": end_date.strftime("%Y-%m-%d")
+        }
+
+        response = requests.post(f"{API_URL}/monthly-analytics/", json=payload)
+        if response.status_code == 200:
+            result = response.json()
+            df = pd.DataFrame(result)
+
+            if not df.empty:
+                pivot = df.pivot(index="month", columns="category", values="total").fillna(0)
+                st.title("Monthly Expense Summary")
+                st.bar_chart(pivot)
+                st.dataframe(pivot.style.format("{:.2f}"))
+            else:
+                st.info("No data found for the selected date range.")
+        else:
+            st.error("Failed to fetch monthly analytics.")
